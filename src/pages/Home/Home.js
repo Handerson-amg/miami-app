b85deb90489d3fa6f6cb2a2f2d58ee8f1f252609
zenge <69840318+Handerson-amg@users.noreply.github.com>
2023-01-13 12:34:47 +0300

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,13 +2,14 @@ import { Typography } from '@mui/material'
 import Page from 'material-ui-shell/lib/containers/Page'
 import React from 'react'
 import { useIntl } from 'react-intl'
+import {avatar} from 'mui/material/avatar'
 
 const HomePage = () => {
   const intl = useIntl()
 
   return (
-    <Page pageTitle={intl.formatMessage({ id: 'home' })}>
-      <Typography>{intl.formatMessage({ id: 'home' })}</Typography>
+    <Page pageTitle={intl.formatMessage({ id: 'SPYDOCS' })}>
+      <Typography>{intl.formatMessage({ id: 'SPYDOCS' })}</Typography>
     </Page>
   )
 }
